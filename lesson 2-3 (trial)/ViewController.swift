42e7cc7cf5abecafb4ef54299376c1d690f975fe
«AslanToktogulov» <aslan-toktogulov@mail.ru>
2024-01-15 20:44:12 +0600

    Initial Commit

---
+++ lesson 2-3 (trial)/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  lesson 2-3 (trial)
+//
+//  Created by Aslan Toktogulov on 15/1/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
